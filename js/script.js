792c7f53d3ad2320c31b372c803814a1d1bf42ce
Adota2107 <nhatminh21072003@gmail.com>
2023-06-19 23:51:56 +0700

    new file:   css/loginstyle.css 	modified:   js/script.js 	new file:   login.html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 function toggleMenu() {
-    // document.getElementById("page-site-index").style.marginLeft = "285px"
     document.getElementById("page-site-index").classList.toggle("marginLeft")
     document.getElementById("nav-drawer").classList.toggle("open")
 }
